b6985be760168345b8eee62b6d0bdf55aa160656
derryev <derryev@ssfs.org>
2023-11-02 18:31:43 -0400

    Completed validation.py.

--- validation.py
+++ validation.py
@@ -1,10 +1,15 @@
 def get_input():
     """
-    This function ensures that a user correctly enters in a number in the proper range.
+    This function ensures that a user correctly enters a number in the proper range.
     :return: a value between 1 and 10, inclusive
     """
-    pass  # make sure to delete this line after you complete your function
-
+    while True:
+        user_choice = input("Please choose a number between 1 and 10: ")
+        if float(user_choice) < 1 or float(user_choice) > 10:
+            print("Don't forget! You need to enter a number between 1 and 10, not "+str(user_choice))
+        elif float(user_choice) >= 1 or float(user_choice) <= 10:
+            break
+    return user_choice
 
 def main():
     print(get_input())
