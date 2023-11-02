5459502f039809e6a40cd17179dd327157370472
derryev <derryev@ssfs.org>
2023-11-02 18:19:59 -0400

    Completed average.py

--- average.py
+++ average.py
@@ -1,19 +1,16 @@
 
 counter=0
 total=0
-
-while True
-     user_choice = input("Enter a number or q to quit")
-     if user_choice=="q":
-         print
-         break
-     you entered
-     the sum of your numbers is
-     the average us
-
-
-
-
-
-counter+=1
-total += int(user_choice)
+print("In this program, you can enter as many numbers as you'd like to find their average")
+while True:
+    user_choice = input("Enter a number or q to quit: ")
+    if user_choice=="q":
+        if counter==0:
+            print("Goodbye :(")
+            break
+        print("You entered "+str(counter)+" numbers")
+        print("Your total is "+str(total))
+        print("The average of your entered numbers is "+str((total/counter)))
+        break
+    counter += 1
+    total += int(user_choice)
